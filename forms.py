c490c36fcf3e416e33a083cbdf34628460a8341f
goumiaoqiong <goumiaoqiong@goumiaoqiongdeMacBook-Pro.local>
2026-01-15 22:11:42 +0800

    add code

--- forms.py
+++ forms.py
@@ -8,6 +8,7 @@ from wtforms.validators import DataRequired, Length
 class TodoListForm(FlaskForm):
     title = StringField('标题', validators=[DataRequired(), Length(1, 64)])
     status = RadioField('是否完成', validators=[DataRequired()],  choices=[("1", '是'),("0",'否')])
+    deadline = StringField('截止时间 (YYYY-MM-DD HH:MM)', validators=[])
     submit = SubmitField('提交')
 
 
@@ -15,3 +16,10 @@ class LoginForm(FlaskForm):
     username = StringField('用户名', validators=[DataRequired(), Length(1, 24)])
     password = PasswordField('密码', validators=[DataRequired(), Length(1, 24)])
     submit = SubmitField('登录')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 24)])
+    password = PasswordField('密码', validators=[DataRequired(), Length(1, 24)])
+    password_confirm = PasswordField('确认密码', validators=[DataRequired(), Length(1, 24)])
+    submit = SubmitField('注册')
